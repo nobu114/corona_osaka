c67e1af4f206ceae562c796498c93e7a42033a05
naohiro <nao114computer@gmail.com>
2020-04-05 20:10:47 +0900

    db修正

--- app/app.py
+++ app/app.py
@@ -13,4 +13,9 @@ def hello():
 @app.route("/index")
 def index():
     all_data = corona_data.query.all()
+    for data in all_data:
+        print(data.place)
+        print(type(data.place))
+    # print(place)
+    # print(type(place))
     return render_template("index.html", all_data=all_data)
