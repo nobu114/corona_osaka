212e2b19350a2251f6523ec5c225aa19173a7809
naohiro <nao114computer@gmail.com>
2020-04-09 18:22:22 +0900

    進捗

--- models/models.py
+++ models/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import (
-    Column, Integer, Text
+    Column, Integer, Text, DateTime
 )
 from models.database import Base
 
@@ -7,16 +7,20 @@ from models.database import Base
 class corona_data(Base):
     __tablename__ = "corona_data"
     index = Column(Integer, primary_key=True)
+    publish_d = Column(DateTime)
     age = Column(Text)
     gender = Column(Text)
     place = Column(Text)
-    date_of_onset = Column(Text)
+    date_of_onset = Column(DateTime)
     symptoms = Column(Text)
     hospitalization = Column(Text)
 
     def __init__(
-            self, age=None, gender=None, place=None, date_of_onset=None,
-            symptoms=None, hospitalization=None):
+            self, index=index, publish_d=None, age=None, gender=None,
+            place=None, date_of_onset=None, symptoms=None,
+            hospitalization=None):
+        self.index = index
+        self.publish_d = publish_d
         self.age = age
         self.gender = gender
         self.place = place
--- models/update.py
+++ models/update.py
@@ -24,22 +24,30 @@ def update_database():
     r_list = []
     for r in range(3, ws.max_row + 1):
         r_tpl = ()
-        for c in range(3, ws.max_column):
+        for c in range(1, ws.max_column):
             value = ws.cell(r, c).value
-            if isinstance(value, datetime.datetime):
+            if c == 2:
+                value = datetime.date(
+                    year=1900, month=1, day=1
+                ) + datetime.timedelta(days=value - 2)
+            elif isinstance(value, datetime.datetime):
                 value = value.date()
             r_tpl += (value, )
         r_list.append(r_tpl)
     items = []
+    """
     init_db()
     for i in range(0, ws.max_row - 2):
         data = corona_data()
-        data.age = r_list[i][0]
-        data.gender = r_list[i][1]
-        data.place = r_list[i][2]
-        data.date_of_onset = r_list[i][3]
-        data.symptoms = r_list[i][4]
-        data.hospitalization = r_list[i][5]
+        data.index = r_list[i][0]
+        data.publish_d = r_list[i][1]
+        data.age = r_list[i][2]
+        data.gender = r_list[i][3]
+        data.place = r_list[i][4]
+        data.date_of_onset = r_list[i][5]
+        data.symptoms = r_list[i][6]
+        data.hospitalization = r_list[i][7]
         items.append(data)
     db_session.add_all(items)
     db_session.commit()
+    """
